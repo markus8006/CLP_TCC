92c6a4a32f8293739f17f43abb750ca3bdaae599
remendoskt <giovannicontegomes@gmail.com>
2025-08-15 17:19:05 -0300

    alterando rotas do html

--- Site/main.py
+++ Site/main.py
@@ -1,28 +1,31 @@
+import json
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 def carregar_clps():
-    with open("dados.json", "r", encoding = "utf-8") as f:
-        return json.load(f)
+    with open("../logs/dados.json", "r", encoding = "utf-8") as f:
+        dados = json.load(f)
+
+    #alterar json colocando "ip" e "portas"
     clps = []
     for ip, portas in dados.items():
         clps.append({"ip": ip, "portas": portas})
     return clps
-    
+
 @app.route('/')
 def index():
     clps = carregar_clps()
     return render_template('index.html', clps = clps)
 
-@app.route('/dados_clps')
-def dados_clps():
-    clps_detectados = [
-        {"ip": "192.168.0.10"},
-        {"ip": "192.168.0.11"}
-    ]
-    return jsonify(clps_detectados)
+@app.route('/clp/<ip>')
+def detalhes_clps(ip):
+    clps = carregar_clps()
+    clp = next((c for c in clps if c["ip"] == ip), None)
+    if clp is None:
+        return "CLP não encontrado", 404
+    return render_template("detalhes.html", clp = clp)
 
 if __name__ == '__main__':
     # Para abrir no navegador local
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader = True)
