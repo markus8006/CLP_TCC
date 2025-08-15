f3ae44fa9db545266795a235c0e58dd1f44008bb
remendoskt <giovannicontegomes@gmail.com>
2025-08-15 15:37:12 -0300

    alterando json

--- Site/main.py
+++ Site/main.py
@@ -4,18 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dados_maquina = {
-        "temperatura": 35.2,
-        "vibracao": 0.05,
-        "corrente": 18.7,
-        "pressao": 12.3
-    }
-
     clps_detectados = [
         {"ip": "192.168.0.10", "portas": [502, 80]},
         {"ip": "192.168.0.11", "portas": [502]},
     ]
-    return render_template('index.html', dados=dados_maquina, clps = clps_detectados)
+    return render_template('index.html', clps = clps_detectados)
 
 @app.route('/dados_clps')
 def dados_clps():
